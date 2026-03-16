d9ee39d4867f3b8719bae9475eccefe5911cc306
Purvbhuva <146190961+Purvbhuva@users.noreply.github.com>
2026-03-16 11:15:20 +0530

    Lab-13Nest

--- Lab-13/src/app.module.ts
+++ Lab-13/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CourseController } from './course.controller';
 import { StudentController } from './student.controller';
 import { TeacherController } from './teacher.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  controllers: [StudentController, TeacherController, CourseController],
+  controllers: [StudentController, TeacherController, CourseController, UsersController],
 })
 export class AppModule {}
